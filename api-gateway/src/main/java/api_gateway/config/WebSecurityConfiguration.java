6a589b74a93ab1980f9703e5fa60981b983de43c
Munot Gadale <munot@cdac.in>
2025-03-17 10:33:29 +0530

    Config commented

--- api-gateway/src/main/java/api_gateway/config/WebSecurityConfiguration.java
+++ api-gateway/src/main/java/api_gateway/config/WebSecurityConfiguration.java
@@ -1,34 +1,34 @@
-package api_gateway.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-
-
-@Configuration
-@EnableWebFluxSecurity
-public class WebSecurityConfiguration {
-
-    private static final String JWK_SET_URI = "http://localhost:8080/realms/keycloak-app/protocol/openid-connect/certs";
-
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange -> exchange
-                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .pathMatchers(HttpMethod.GET, "/public/**").permitAll()
-                        .pathMatchers("/eureka/**").permitAll()
-                        .pathMatchers("/user-management/**").authenticated()
-                        .pathMatchers("/product-management/**").authenticated()
-                        .pathMatchers("/dms/**").authenticated()
-                        .pathMatchers("/notification-management/**").authenticated()
-                        .anyExchange().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
-        return serverHttpSecurity.build();
-    }
-
-}
+//package api_gateway.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class WebSecurityConfiguration {
+//
+//    private static final String JWK_SET_URI = "http://localhost:8080/realms/keycloak-app/protocol/openid-connect/certs";
+//
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchange -> exchange
+//                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                        .pathMatchers(HttpMethod.GET, "/public/**").permitAll()
+//                        .pathMatchers("/eureka/**").permitAll()
+//                        .pathMatchers("/user-management/**").authenticated()
+//                        .pathMatchers("/product-management/**").authenticated()
+//                        .pathMatchers("/dms/**").authenticated()
+//                        .pathMatchers("/notification-management/**").authenticated()
+//                        .anyExchange().authenticated())
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+//        return serverHttpSecurity.build();
+//    }
+//
+//}
